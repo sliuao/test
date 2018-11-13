06b3cc5db2dbbdd1ad6b355ccda8a9c3114690ee
刘殊玥 <liushuyue@liushuyuedeMacBook-Pro.local>
2018-11-13 20:36:34 +0800

    1

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, url_for, render_template, request, redirect, session, jsonify, make_response, send_from_directory
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
